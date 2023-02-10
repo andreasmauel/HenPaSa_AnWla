c62035807783db08d5cbe414abb1b605a1c01db6
SchmidtWla <wladislaw.schmidt14@gmail.com>
2023-02-10 10:19:13 +0100

    updated pom.xml

--- src/main/java/equipment/Equipment.java
+++ src/main/java/equipment/Equipment.java
@@ -7,4 +7,36 @@ public abstract class Equipment {
     private float weigth;
     private int requiredStrength;
 
+
+    public Attribute getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(Attribute attribute) {
+        this.attribute = attribute;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(float weigth) {
+        this.weigth = weigth;
+    }
+
+    public int getRequiredStrength() {
+        return requiredStrength;
+    }
+
+    public void setRequiredStrength(int requiredStrength) {
+        this.requiredStrength = requiredStrength;
+    }
 }
--- src/main/java/equipment/weapon/Dagger.java
+++ src/main/java/equipment/weapon/Dagger.java
@@ -1,9 +1,19 @@
 package equipment.weapon;
 
+import equipment.Attribute;
+
 public class Dagger extends Weapon{
 
     public Dagger() {
-        setDamage();
+        //TODO Würfel später hinzufügen bzw damage
+        setAttribute(Attribute.STRENGTH);
+        setMultiAttackAllowed(false);
+        setPrice(2);
+        setWeigth(1);
+        setRange(1);
+        setThrowable(false);
+        setRequiredStrength(0);
+
     }
     protected void draw() {
 
