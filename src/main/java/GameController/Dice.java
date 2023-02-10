ff1d05bd8f22617f0c8d4368ea273a0295d5221f
tecncikal <hendrik@tf115.de>
2023-02-10 11:21:18 +0100

    Dice implementieren (begonnen

---
+++ src/main/java/GameController/Dice.java
@@ -0,0 +1,15 @@
+package GameController;
+
+public class Dice {
+
+    public enum DiceSites{
+        FOUR, EIGHT, TEN, TWENTY;
+
+        private DiceSites(){
+            this.diceSites = EIGHT;
+        }
+    }
+    public int random() {
+
+    }
+}
