03a63325c8bf8fb286aec4f1bb335bf588cf6266
tecncikal <hendrik@tf115.de>
2023-02-16 09:36:17 +0100

    Dice implementieren (begonnen

---
+++ src/main/java/GameController/DiceTwenty.java
@@ -0,0 +1,46 @@
+package GameController;
+
+import java.util.Random;
+
+public class DiceTwenty implements IDice {
+
+    private DiceSites diceSites;
+
+
+    public DiceTwenty() {
+        this.diceSites = DiceSites.TWENTY;
+    }
+
+
+    public int rollDice() {
+        Random random = new Random();
+        if (diceSites.equals(DiceSites.FOUR)){
+            return random.nextInt(1, 5);
+        } else if(diceSites.equals(DiceSites.EIGHT)) {
+            return random.nextInt(1, 9);
+        } else if(diceSites.equals(DiceSites.TEN)){
+            return random.nextInt(1, 11);
+        } else {
+            return random.nextInt(1, 21);
+        }
+
+    }
+
+    public int rollDiceAdvantage(){
+        int x = rollDice(), y = rollDice();
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public int rollDiceDisadvantage(){
+        int x = rollDice(), y = rollDice();
+        if (x > y) {
+            return y;
+        } else {
+            return x;
+        }
+    }
+}
---
+++ src/main/java/GameController/IDice.java
@@ -0,0 +1,10 @@
+package GameController;
+
+public interface IDice {
+
+    public int rollDice();
+
+    public int rollDiceAdvantage();
+
+    public int rollDiceDisadvantage();
+}
