a0cf5428e9a375d3172f340708d19203e46d7654
SchmidtWla <wladislaw.schmidt14@gmail.com>
2023-02-16 09:18:20 +0100

    added last weapons and a few armor

---
+++ src/main/java/equipment/armor/LeatherArmor.java
@@ -0,0 +1,11 @@
+package equipment.armor;
+
+import util.Attribute;
+
+public class LeatherArmor extends Armor{
+
+
+    public LeatherArmor(Attribute attribute, float price, float weigth, int requiredStrength, int armorClass, String drawback) {
+        super(Attribute.DEXTERITY, 10, 10, 0, 1, "");
+    }
+}
---
+++ src/main/java/equipment/armor/NoArmor.java
@@ -0,0 +1,9 @@
+package equipment.armor;
+
+import util.Attribute;
+
+public class NoArmor extends Armor{
+    public NoArmor(Attribute attribute, float price, float weigth, int requiredStrength, int armorClass, String drawback) {
+        super(attribute, price, weigth, requiredStrength, armorClass, drawback);
+    }
+}
