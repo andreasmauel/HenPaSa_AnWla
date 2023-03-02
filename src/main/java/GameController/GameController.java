073c3fd288a6859557523e64a794a02e8bb2889c
andreasmauel <andreas.mauel.bwv@gmail.com>
2023-03-02 10:51:06 +0100

    initial commit

---
+++ src/main/java/GameController/GameController.java
@@ -0,0 +1,32 @@
+package GameController;
+
+import battlemap.Dungeon.Dungeon;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GameController
+{
+    private ArrayList<Player> players = new ArrayList<Player>();
+    private ArrayList<Player> monster = new ArrayList<Player>();
+    private Dungeon dungeon;
+    private HashMap<String, IDice> dices = new HashMap<String, IDice>();
+    GameController(){
+        dices.put("four", new DiceFour());
+        dices.put("eight", new DiceEight());
+        dices.put("ten", new DiceTen());
+        dices.put("twenty", new DiceTwenty());
+    }
+
+    public void gameStart(){
+
+    }
+
+    public void roundStart(){
+
+    }
+
+    public void roundEnd(){
+
+    }
+}
