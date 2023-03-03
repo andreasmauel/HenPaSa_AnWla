2cfa34bbd36d8ed45fda2e2f4ca7da778e3c3051
tecncikal <hendrik@tf115.de>
2023-03-03 10:04:07 +0100

    Character Refactor

--- src/main/java/Characters/Races/Dwarf.java
+++ src/main/java/Characters/Races/Dwarf.java
@@ -1,14 +1,12 @@
 package Characters.Races;
 
+import Characters.Player;
+
 public class Dwarf extends Race{
     private int intelligenceModifier = 0;
     private int dexterityModifier = 0;
     private int strengthModifier = 2;
     private int constitutionModifier = 2;
     private int wisdomModifier = 1;
-
-    public void applyModifier(){
-
-    }
 
 }
