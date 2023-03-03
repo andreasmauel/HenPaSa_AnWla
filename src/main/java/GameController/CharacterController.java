1f6e6bbbda39df728b71401afda72302a70a8878
andreasmauel <andreas.mauel.bwv@gmail.com>
2023-03-03 10:37:22 +0100

    Character Controller

--- src/main/java/GameController/CharacterController.java
+++ src/main/java/GameController/CharacterController.java
@@ -1,4 +1,23 @@
 package GameController;
 
 public class CharacterController {
+    public void createPlayerCharacter(){
+
+    }
+
+    private void rollAttributes(){
+
+    }
+
+    private void assignAttributesToAbility(){
+
+    }
+
+    private void chooseClass(){
+
+    }
+
+    private void chooseRace(){
+
+    }
 }
