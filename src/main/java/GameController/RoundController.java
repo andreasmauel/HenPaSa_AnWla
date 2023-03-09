9d83988a65f6155f748addaf4a7b733147d9f7bc
freaksheep <freaksheep90@gmail.com>
2023-03-09 09:07:47 +0100

    game controller

--- src/main/java/GameController/RoundController.java
+++ src/main/java/GameController/RoundController.java
@@ -1,11 +1,35 @@
 package GameController;
 
+import Characters.Monster;
 import Characters.Player;
 
 public class RoundController {
-    RoundController() {
 
+    private Player[] players;
+    private Monster[] monsters;
+
+    RoundController(Player[] players, Monster[] monsters) {
+        this.setPlayers(players);
+        this.setMonsters(monsters);
     }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public Monster[] getMonsters() {
+        return monsters;
+    }
+
+    public void setMonsters(Monster[] monsters) {
+        this.monsters = monsters;
+    }
+
+    public void
 
     private void playerAction(Player[] players) {
 
