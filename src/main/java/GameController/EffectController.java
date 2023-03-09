d165bd731a4b8fe09a84deaff3505c4147c92e23
SchmidtWla <wladislaw.schmidt14@gmail.com>
2023-03-09 09:11:31 +0100

    added test, activate and getter methods

--- src/main/java/GameController/EffectController.java
+++ src/main/java/GameController/EffectController.java
@@ -19,22 +19,45 @@ public class EffectController {
 
     public String triggerEffect(Effect effect, int stat) {
 
-        switch(effect) {
-            case DAMAGE -> this.character.setLife(this.character.getLife - stat );
-            case HEAL -> this.character.setLife(this.character.getLife + stat);
-            case SLEEP -> this.character.skipRounds(stat);
-            case ARMOR -> this.character.setCurrentArmorClass(this.character.getCurrentArmorClass() + stat);
-        }
-
+//        switch(effect) {
+//            case DAMAGE -> this.character.setLife(this.character.getLife - stat );
+//            case HEAL -> this.character.setLife(this.character.getLife + stat);
+//            case SLEEP -> this.character.skipRounds(stat);
+//            case ARMOR -> this.character.setCurrentArmorClass(this.character.getCurrentArmorClass() + stat);
+//        }
+        return null;
     }
 
     public void addEffect(Effect effect, int duration) {
-        if(this.effects.get(effect) == null) {
+        if (this.effects.get(effect) == null) {
             this.effects.put(effect, duration);
         } else {
             int x = this.effects.get(effect);
             x += duration;
             this.effects.replace(effect, x);
+        }
+    }
+
+    public Map<Effect, Integer> activateEffects() {
+
+        Map<Effect, Integer> effectsCopy = new HashMap<Effect, Integer>(this.effects);
+        decreaseDuration();
+        return effectsCopy;
+    }
+
+    public Map<Effect, Integer> getEffects() {
+        return effects;
+    }
+
+    private void decreaseDuration() {
+        for (Map.Entry<Effect, Integer> entry : this.effects.entrySet()) {
+            Integer duration = entry.getValue();
+            duration--;
+            if (duration == 0) {
+                this.effects.remove(entry.getKey());
+            } else {
+                entry.setValue(duration);
+            }
         }
     }
 }
