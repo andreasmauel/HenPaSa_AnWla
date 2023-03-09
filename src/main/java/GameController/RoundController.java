967dd59e4816af58f460c088c6a31e2a63bb6014
freaksheep <freaksheep90@gmail.com>
2023-03-09 11:27:40 +0100

    game controller

--- src/main/java/GameController/RoundController.java
+++ src/main/java/GameController/RoundController.java
@@ -3,39 +3,54 @@ package GameController;
 import Characters.Monster;
 import Characters.Player;
 
+import java.util.ArrayList;
+
 public class RoundController {
 
-    private Player[] players;
-    private Monster[] monsters;
+    private ArrayList<Player> players;
+    private ArrayList<Player> monsters;
 
-    RoundController(Player[] players, Monster[] monsters) {
+    RoundController(ArrayList<Player> players, ArrayList<Player> monsters) {
         this.setPlayers(players);
         this.setMonsters(monsters);
     }
 
-    public Player[] getPlayers() {
+    public ArrayList<Player> getPlayers() {
         return players;
     }
 
-    public void setPlayers(Player[] players) {
+    public void setPlayers(ArrayList<Player> players) {
         this.players = players;
     }
 
-    public Monster[] getMonsters() {
+    public ArrayList<Player> getMonsters() {
         return monsters;
     }
 
-    public void setMonsters(Monster[] monsters) {
+    public void setMonsters(ArrayList<Player> monsters) {
         this.monsters = monsters;
     }
 
-    public void
+    public void performRound() {
+        for (Player player : this.players) {
+            this.playerAction(player);
+            this.checkFight(player);
+        }
+        for (Player monster: this.monsters) {
+            this.monstersAction(monster);
+            this.checkFight(monster);
+        }
+    }
 
-    private void playerAction(Player[] players) {
+    private void monstersAction(Player monster) {
 
     }
 
-    private void checkFight() {
+    private void playerAction(Player player) {
+
+    }
+
+    private void checkFight(Player character) {
 
     }
 }
