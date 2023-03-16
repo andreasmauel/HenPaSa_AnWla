1b65446025b3ba0c685e4533c363fda45f1044c5
andreasmauel <andreas.mauel.bwv@gmail.com>
2023-03-16 08:31:45 +0100

    Game Start

--- src/main/java/GameController/GameStart.java
+++ src/main/java/GameController/GameStart.java
@@ -1,9 +1,21 @@
 package GameController;
 
+import javax.swing.*;
+
 public class GameStart {
     private final String OPENING_DIALOG = "Find all three keys and defeat the ultimate evil";
     private final String CHOOSE_NAME = "Enter your Character's Name";
     private final String CONFIGURE_PLAYER_CHAR = "";
+    private final String GAME_TITLE = "Dungeon Crawler";
     public void run(){
+        gameStartScreen();
+    }
+
+    private void gameStartScreen(){
+        JFrame jFrame = new JFrame(this.GAME_TITLE);
+        jFrame.setSize(1920, 1080);
+        jFrame.setVisible(true);
+        JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
     }
 }
---
+++ src/main/java/GameController/GameStartGui.java
@@ -0,0 +1,10 @@
+package GameController;
+
+import javax.swing.*;
+
+public class GameStartGui {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton spielStartenButton;
+}
