94f16a99efdd5d5b93203f462132d85c5d484008
SchmidtWla <wladislaw.schmidt14@gmail.com>
2023-03-16 09:44:37 +0100

    ...

--- src/main/java/GameController/MonsterController.java
+++ src/main/java/GameController/MonsterController.java
@@ -1,13 +1,24 @@
 package GameController;
 
 import Characters.Monster;
+import Characters.Player;
 
 public class MonsterController {
 
-    Monster monster;
+    Monster[] monsters;
+    Player[] players;
 
-    public MonsterController(Monster monster) {
-        this.monster = monster;
+    public MonsterController(Monster[] monsters, Player[] players) {
+        this.monsters = monsters;
+        this.players = players;
+    }
+
+    public moveToNextPlayer() {
+
+        int distance = 0;
+        for(Player player : players) {
+            player.get
+        }
     }
 
 }
