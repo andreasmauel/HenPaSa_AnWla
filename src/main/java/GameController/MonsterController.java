fd3d4c993e5d971217ae79bbe0e41ec65935dd51
SchmidtWla <wladislaw.schmidt14@gmail.com>
2023-03-16 11:36:35 +0100

    monster controller

--- src/main/java/GameController/MonsterController.java
+++ src/main/java/GameController/MonsterController.java
@@ -1,24 +1,51 @@
 package GameController;
 
+import Characters.AbstractCharacter;
 import Characters.Monster;
 import Characters.Player;
+import battlemap.Dungeon.Dungeon;
+import util.DistanceCalculator;
 
 public class MonsterController {
 
-    Monster[] monsters;
+    Monster monster;
     Player[] players;
+    Dungeon dungeon;
 
-    public MonsterController(Monster[] monsters, Player[] players) {
-        this.monsters = monsters;
+    AbstractCharacter nearestEnemy;
+
+    public MonsterController(Monster monster , Player[] players, Dungeon dungeon) {
+        this.monster = monster;
         this.players = players;
+        this.dungeon = dungeon;
+
     }
 
-    public moveToNextPlayer() {
+    public void attack() {
+      if(distanceToNearestEnemy() < this.monster.getRange()) {
+          this.monster.attack(this.nearestEnemy);
+      }
+    }
 
-        int distance = 0;
+
+    public AbstractCharacter getNearestPlayer() {
+
+        int distance = Integer.MAX_VALUE;
+        AbstractCharacter nearestCharacter = null;
         for(Player player : players) {
-            player.get
+           int distancePlayer = DistanceCalculator.getDistanceBetweenCharacters(player, this.monster);
+           if(distancePlayer < distance) {
+               distance = distancePlayer;
+               this.nearestEnemy = player;
+           }
         }
+        return this.nearestEnemy;
     }
+
+    public int distanceToNearestEnemy() {
+        return DistanceCalculator.getDistanceBetweenCharacters(this.nearestEnemy, this.monster);
+    }
+
+
 
 }
