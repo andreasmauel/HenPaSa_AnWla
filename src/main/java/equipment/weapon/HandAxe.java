eba42e421c9458da7241365e860ffce635e4f34e
SchmidtWla <wladislaw.schmidt14@gmail.com>
2023-03-23 10:15:29 +0100

    fixed dice in handAxe

--- src/main/java/equipment/weapon/HandAxe.java
+++ src/main/java/equipment/weapon/HandAxe.java
@@ -1,13 +1,13 @@
 package equipment.weapon;
 
-import GameController.dices.DiceEight;
+import GameController.dices.DiceSix;
 import util.Attribute;
 
 public class HandAxe extends Weapon {
 
     public HandAxe() {
         //TODO Damage
-        super(Attribute.NOATTRIBUTE,5,2,0,new DiceEight(),true,2,true);
+        super(Attribute.NOATTRIBUTE,5,2,0,new DiceSix(),true,2,true);
     }
     @Override
     protected void draw() {
