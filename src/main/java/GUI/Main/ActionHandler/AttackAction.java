8d800f6bc9e2c116970d570b79c71bc7f5878b9c
SchmidtWla <wladislaw.schmidt14@gmail.com>
2023-03-31 11:15:33 +0200

    ...

--- src/main/java/GUI/Main/ActionHandler/AttackAction.java
+++ src/main/java/GUI/Main/ActionHandler/AttackAction.java
@@ -8,6 +8,7 @@ import GUI.Main.MainFrame;
 import GameController.GameController;
 import GameController.RoundController;
 import battlemap.Dungeon.Dungeon;
+import util.DistanceCalculator;
 
 public class AttackAction extends ActionOption
 {
@@ -23,9 +24,14 @@ public class AttackAction extends ActionOption
     {
         AbstractCharacter enemy = dungeon.getCharacterPos(x,y).getCharacter();
         Player character = this.gameController.getRoundController().getActivePlayer();
-        if(dungeon.isInRange(character, character.getCurrentWeapon().getRange(), x, y)) {
+        if(enemy.getCurrentLifepoints() <= 0) {
+            DialogBox.ConsoleOut(enemy.getName() + enemy.getName() + " " + enemy.getId() + " is already dead. Please no more attacks on this poor little monster!" );
+            return;
+        }
+        if(DistanceCalculator.isRange(character, character.getCurrentWeapon().getRange(), x, y)) {
             int damage = character.attack(enemy);
-            DialogBox.ConsoleOut(enemy.getName() + " takes " + damage + " damage.");
+            DialogBox.ConsoleOut(enemy.getName() + " " + enemy.getId() + " takes " + damage + " damage.");
+            DialogBox.ConsoleOut(enemy.getName() + " " + enemy.getId() + " remaining lifepoints: " + enemy.getCurrentLifepoints());
             return;
         }
         DialogBox.ConsoleOut("Enemy is out of range");
